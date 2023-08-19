5f933e2ee9cb42f0e91112dc0bbcc9998be96600
Fred Duarte <fduartej@gmail.com>
2023-08-18 20:48:21 -0500

    Codigo Inicial

---
+++ Controllers/ContactoController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using petclinic.Models;
+
+namespace petclinic.Controllers
+{
+    public class ContactoController : Controller
+    {
+        private readonly ILogger<ContactoController> _logger;
+
+        public ContactoController(ILogger<ContactoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contacto objContacto)
+        {
+            ViewData["Message"] = string.Concat("Estimado " , objContacto.Name, " te estaremos contactando pronto.");
+            return View("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
