5158ba9315205598fa8714b5161e13719e2848bd
Fred Duarte <fduartej@gmail.com>
2023-09-01 19:51:08 -0500

    Grabando Contacto

--- Controllers/ContactoController.cs
+++ Controllers/ContactoController.cs
@@ -6,16 +6,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using petclinic.Models;
+using petclinic.Data;
 
 namespace petclinic.Controllers
 {
     public class ContactoController : Controller
     {
         private readonly ILogger<ContactoController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public ContactoController(ILogger<ContactoController> logger)
+        public ContactoController(ILogger<ContactoController> logger,
+        ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -26,6 +30,8 @@ namespace petclinic.Controllers
         [HttpPost]
         public IActionResult Create(Contacto objContacto)
         {
+            _context.Add(objContacto);
+            _context.SaveChanges();
             ViewData["Message"] = string.Concat("Estimado " , objContacto.Name, " te estaremos contactando pronto.");
             return View("Index");
         }
